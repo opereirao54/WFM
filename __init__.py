ac5f6411af61426e1c07fd6f181c3b5760930ac5
opereirao54 <opereirao54@gmail.com>
2026-04-18 12:53:45 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from .models import WFMInput, WFMOutput, PausasAdicionais, Turno, Alerta
-from .engine import run_engine
-from .demand import make_bimodal, make_morning, make_flat_business
-
-__all__ = [
-    "WFMInput", "WFMOutput", "PausasAdicionais", "Turno", "Alerta",
-    "run_engine",
-    "make_bimodal", "make_morning", "make_flat_business",
-]
